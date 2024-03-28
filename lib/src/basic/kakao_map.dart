63bc64564b834ef0d9665133582d07bd3de8383e
seunghwanly <seunghwanly@gmail.com>
2024-03-29 01:02:26 +0900

    fix: markers는 marker를 지도에 표기한 뒤 추가

--- lib/src/basic/kakao_map.dart
+++ lib/src/basic/kakao_map.dart
@@ -528,8 +528,6 @@ class _KakaoMapState extends State<KakaoMap> {
       marker.setImage(markerImage);
     }
 
-    markers.push(marker);
-
     let infoWindow = null
     if (infoWindowText !== '' && infoWindowText !== 'null') {
 
@@ -602,6 +600,7 @@ class _KakaoMapState extends State<KakaoMap> {
 
     // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(map);
+    markers.push(marker);
   }
 
   function setMarkerDraggable(markerId, draggable) {
