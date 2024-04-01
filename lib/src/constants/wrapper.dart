52d02df58cfd305215fe6110f5df0ac29ec7e638
seunghwanly <seunghwanly@gmail.com>
2024-04-01 15:21:22 +0900

    fix: set font-family to 'Pretendard'

--- lib/src/constants/wrapper.dart
+++ lib/src/constants/wrapper.dart
@@ -9,6 +9,13 @@ String _htmlWrapper(String script) {
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
   <script type="text/javascript"
           src='https://dapi.kakao.com/v2/maps/sdk.js?autoload=true&appkey=${AuthRepository.instance.appKey}&libraries=services,clusterer,drawing'></script>
+  <link rel="stylesheet" as="style" crossorigin href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable.css" />
+
+  <style>
+    * {
+      font-family: 'Pretendard';
+    }
+  </style>
 </head>
 
 <body style="margin: 0;">
